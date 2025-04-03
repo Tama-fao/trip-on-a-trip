5b32a4fdee981d8805e142718202a13c4d0c4d0f
Tama-fao <tamara.faoro@stud.hslu.ch>
2025-04-03 11:02:51 +0200

    muliple character script change so it does not crash on itself

--- Assets/Scripts/MultipleCharacters.cs
+++ Assets/Scripts/MultipleCharacters.cs
@@ -14,20 +14,20 @@ namespace LLMUnitySamples
         int interactionCount = 0;
         const int maxInteractions = 3;
 
-
         ElevenlabsAPI api;
-
         string voice_id;
+        AudioPlaybackHandler audioHandler;
 
-        public MultipleCharactersInteraction(InputField playerText, Text AIText, LLMCharacter llmCharacter, System.Action<string> onResponseComplete,  string voice_id,
+        public MultipleCharactersInteraction(InputField playerText, Text AIText, LLMCharacter llmCharacter, System.Action<string> onResponseComplete, string voice_id,
             ElevenlabsAPI api = null)
         {
             this.playerText = playerText;
             this.AIText = AIText;
             this.llmCharacter = llmCharacter;
             this.onResponseComplete = onResponseComplete;
-                 this.api = api;
+            this.api = api;
             this.voice_id = voice_id;
+            this.audioHandler = api?.GetComponent<AudioPlaybackHandler>();
         }
 
         public void Start()
@@ -40,7 +40,6 @@ namespace LLMUnitySamples
         {
             playerText.interactable = false;
             AIText.text = "...";
-            interactionCount = 0;
             ProcessInteraction(message);
         }
 
@@ -50,52 +49,67 @@ namespace LLMUnitySamples
             {
                 _ = llmCharacter.Chat(message, SetAIText, () => onChatComplete(message));
             }
+            else
+            {
+                // Reset counter when max interactions reached
+                interactionCount = 0;
+                playerText.interactable = true;
+                playerText.Select();
+                playerText.text = "";
+            }
         }
-
 
         public void SetAIText(string text)
         {
             AIText.text = text;
         }
 
-
-             public async void onChatComplete(string message)
+        public async void onChatComplete(string message)
         {
-            interactionCount++;
-
-            api.AudioReceived.AddListener(OnAudioReceived);
-            api.GetAudio(AIText.text, voice_id);
-
-
+            if (api != null)
+            {
+                api.AudioReceived.AddListener(OnAudioReceived);
+                api.GetAudio(AIText.text, voice_id);
+            }
+            else
+            {
+                await Task.Delay(500); // Small delay if no audio
+                CompleteInteraction();
+            }
         }
 
- private void OnAudioReceived(AudioClip audioClip)
+        private void OnAudioReceived(AudioClip audioClip)
         {
-            var audioHandler = api.GetComponent<AudioPlaybackHandler>();
+            api.AudioReceived.RemoveListener(OnAudioReceived); // Remove listener immediately
+            
             if (audioHandler != null)
             {
                 audioHandler.PlayAudioClip(audioClip);
-                WaitForAudioToFinish(audioHandler).ConfigureAwait(false);
+                WaitForAudioToFinish().ConfigureAwait(false);
+            }
+            else
+            {
+                CompleteInteraction();
             }
         }
-        private async Task WaitForAudioToFinish(AudioPlaybackHandler audioHandler)
-        {
-            while (audioHandler.IsPlaying())
-            {
-                await Task.Yield(); // Yield control back to the main thread
-            }
 
-            playerText.interactable = true;
-            playerText.Select();
-            playerText.text = "";
+        private async Task WaitForAudioToFinish()
+        {
+            while (audioHandler != null && audioHandler.IsPlaying())
+            {
+                await Task.Yield();
+            }
+            CompleteInteraction();
+        }
+
+        private void CompleteInteraction()
+        {
+            interactionCount++; // Increment counter here instead
             onResponseComplete(AIText.text);
         }
-
-
-
     }
 
-   public class MultipleCharacters : MonoBehaviour
+    public class MultipleCharacters : MonoBehaviour
     {
         public LLMCharacter llmCharacter1;
         public InputField playerText1;
@@ -115,17 +129,19 @@ namespace LLMUnitySamples
         void Start()
         {
             interaction1 = new MultipleCharactersInteraction(playerText1, AIText1, llmCharacter1, ResponseFromCharacter1, voiceId1, _elevenLabsAPI);
-            interaction2 = new MultipleCharactersInteraction(null, AIText2, llmCharacter2, ResponseFromCharacter2, voiceId2, _elevenLabsAPI);
+            interaction2 = new MultipleCharactersInteraction(playerText2, AIText2, llmCharacter2, ResponseFromCharacter2, voiceId2, _elevenLabsAPI);
             interaction1.Start();
         }
 
         void ResponseFromCharacter1(string response)
         {
+            // Don't reset playerText1 here - let the interaction handle it
             interaction2.ProcessInteraction(response);
         }
 
         void ResponseFromCharacter2(string response)
         {
+            // Don't reset playerText2 here - let the interaction handle it
             interaction1.ProcessInteraction(response);
         }
 
